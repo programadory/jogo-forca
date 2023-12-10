0151585f58669a0623293ccabe07c15cee06897e
programadory_android <programadory@gmail.com>
2023-12-10 18:27:54 -0300

    forca.cpp

--- forca.cpp
+++ forca.cpp
@@ -2,21 +2,26 @@
 
 using namespace std;
 
-#define CLEAR system("clear");
+#define CLEAR system("clear"); //Macro to clean up terminal contend.
 
 int main(){
+  
+  /* 1th block of variables */
   
   string topic="", word="";
   char char_letter;
   int word_sz=0, it=0;
   bool check=1;
- 
+  
+  /* Catching data from user */
+  
   cout << "-----JOGO DA FORCA-----\n\n";
   cout << "Palavra: ";
   cin >> word;
   CLEAR
   
-  //String length calc
+  /* String lengh calculator */
+  
   for(; it<sizeof(word) || check==true; it++){
       if(word[it]=='\0'){
         word_sz+=it;
@@ -24,38 +29,98 @@ int main(){
         break;
       }
   }
+
+  /* Allocating dynamic memory to our main variables. */
   
-  char* word_overwrite = (char*) malloc(word_sz);
+  char* word_overwrite = (char*) malloc(word_sz); 
   char* word_show = (char*) malloc(word_sz);
+  
+  /* Filling up variable with animation purposes */
   
   for(int letters=0; letters<word_sz ; letters++){
       word_overwrite[letters] = (word[letters]=='-') ? '-' : '_';
   }
   
+  /* Stickman drawing */
+  
+  string stickman[9]= { 
+    " .------\n",
+    " |  (+_+)\n", //life 5
+    " |  / | ",    //life 4
+    "/\n",         //life 3
+    " |    | \n",  //life 2
+    " |   * *\n",  //life 1
+    " |  |   |\n", //life 0
+    " |      \n",  
+    "---     \n"
+    
+  };
+  
+  /* Hang drawing */
+  
+  string stickman_overwrite[9]= { 
+    " .------\n",
+    " |  \n", 
+    " |  ",  
+    "\n",  
+    " |    \n",  
+    " |   \n",  
+    " |  \n", 
+    " |      \n", 
+    "---     \n"  
+    
+  };
+  
+  /* 2th block of variables, game variables */
+  
   it=0;
+  int i3=0;
   int lt=0, score=0;
   int life=6;
-  bool gameover=0, game=1;
+  bool gameover=0;
+
+  system("stty cbreak"); //Disable wait-till-Enter-click on Termux
   
-  system("stty cbreak");
+  /* Main looping */
+  
   do{
-    
-      CLEAR // Revisar sobreposicao de caracter word_show
-      cout << "Score: " << score << "          Remain life: " << life << "\n\nTries: " << word_show  << "\n\n" << "\n\n\n\n\n\n\n\n" << word_overwrite << "\n";
+     
+      CLEAR
       
-      //Compara lt com word para a condicao de vitoria
+      /*Printing game contend on screen */
+      
+      cout << "Score: " << score << "          Remain life: " << life << "\n\nTries: " << word_show  << "\n\n";
+      
+      /* Drawing hangman */
+      
+      for(int i=0; i<9; i++){ //Desenho do boneco vazio.
+        cout << stickman_overwrite[i];
+      }
+      
+      /* Win or Lose statements */
+      
       if(lt==word_sz){
-        cout << "VOCE VENCEU O JOGO!\n";
+        cout << "VOCE VENCEU O JOGO!\n\n";
         break;
       }
       
       if(life==0){
-        cout << "VOCE PERDEU!\n";
+        cout << "VOCE PERDEU!\n\n";
         break;
       }
       
+      /* Printing line animation */ 
+      
+      cout << "\n\n\n\n\n\n\n\n" << word_overwrite << "\n";
+      
+      
+      
+      /* User hangman game input */
+      
       cin >> char_letter;
- 
+      
+      /* Iterating all over the array word[] and word_overwrite[] looking for hints, if hint then the character is added to word[] array then return a lot of variable changes, as score, lt that represents ( - ) character, predicting an error from user and the condition not be satisfied. */
+      
       for(int i=0; i<word_sz; i++) 
         {
           
@@ -65,21 +130,32 @@ int main(){
             lt++;
             gameover=true;
           }
-          //CondicaodeWIN verifica se a forca ja esta completa e retorna lt++;
+          
           if(lt!=word_sz && word_overwrite == word){
             lt++;
           }
           
         }
       
+      /* Statement due to a no-equal input from user, decreasing and increasing life, stickman to overwrite the current drawer according to the life counting. */
+      
       if(!gameover){
-          word_show[it++] = char_letter;
+        if(i3==0){ //Fixing the drawing start point.  
+          i3++;
+        }
+        stickman_overwrite[i3] = stickman[i3];
+        
+        word_show[it++] = char_letter;
         --life;
+        i3++;
       }
+      
+      /* Reseting gameover statement */    
       
       gameover=false;
       
   }while(true);
+  
   system("stty -cbreak");
   
   return 0;
